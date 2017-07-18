1b5ee6b5ffd4551756f5cf929d7cff9cc996fdfd
Igor de Medeiros <igormedeiros0@gmail.com>
2017-07-18 15:09:19 -0300

    create and nest the search bar as a functional component

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//Functional Component
+const SearchBar = () => {
+    return <input />; // React.createElement
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyA2UbDXh2uxyL2ygBkAjjFeE1lKz6udBNY';
 
 const App = () => {
-    return <div>Hi!</div>
-}
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    )
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
